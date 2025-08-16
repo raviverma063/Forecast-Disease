67e8a96f3c1bf1192f5c4468b17cc11fb658e55e
raviverma063 <54953874+raviverma063@users.noreply.github.com>
2025-08-16 15:13:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
-import HospitalLocator from '@/components/HospitalLocator';
 import PageHeader from '@/components/page-header';
 import LiveRiskRadar from '@/components/LiveRiskRadar';
-import InteractiveVisualization from '@/components/dashboard/interactive-visualization';
 import DiseaseTrendChart from '@/components/dashboard/disease-trend-chart';
+import HospitalLocator from '@/components/HospitalLocator';
 
 export default function DashboardPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+    <div className="flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <PageHeader title="Dashboard" />
       <div className="grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
-        <div className="xl:col-span-2">
-         <LiveRiskRadar />
-        </div>
-        <div className="lg:col-span-2 xl:col-span-1">
-          <InteractiveVisualization />
+        <div className="lg:col-span-2 xl:col-span-3">
+          <LiveRiskRadar />
         </div>
         <div className="lg:col-span-2 xl:col-span-3">
           <DiseaseTrendChart />
+        </div>
+        <div className="lg:col-span-2 xl:col-span-3">
+          <HospitalLocator />
         </div>
       </div>
     </div>
